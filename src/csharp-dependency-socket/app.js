efbacb712f140c4c66d5c3795b47dd93f9a449e0
unknown <faruk_thecno@hotmail.com>
2020-03-20 22:20:11 +0300

    Socket IO

--- src/csharp-dependency-socket/app.js
+++ src/csharp-dependency-socket/app.js
@@ -1,3 +1,17 @@
-'use strict';
+var io = require('socket.io')(8081)
 
-console.log('Hello world');
+io.on("connection", (socket) => {
+    console.log('Connecteded:', socket.id);
+
+    socket.on('disconnect', () => {
+        console.log('Disconnected:', socket.id);
+    });
+
+    socket.on('visualizedependency', (repo) => {
+        console.log(repo);
+    })
+})
+
+io.httpServer.on('listening', function () {
+    console.log('listening on port', io.httpServer.address().port);
+})
